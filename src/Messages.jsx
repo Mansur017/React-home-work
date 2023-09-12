8d44c7373a936c62d03c4843a8f6b955a5dc1516
Mansur017 <mansurtovs017@icloud.com>
2023-09-12 18:12:44 +0300

    deleteMessage

---
+++ src/Messages.jsx
@@ -0,0 +1,62 @@
+import React, { Fragment, useState } from "react";
+
+
+
+
+function Messages() {
+
+const [messages, setMessages] = useState([
+    {
+        text: "Hello!",
+        author: "Adam",
+        side: "left"
+    },
+    {
+        text: "Hi!",
+        author: "Ali",
+        side: "right"   
+    },
+    {
+        text: "How are you?",
+        author: "Adam",
+        side: "left"
+    },
+    {
+        text: "I'm fine",
+        author: "Ali",
+        side: "right"
+    },
+        
+    ])
+
+const deleteMessage = (index) => {
+const messageCopy = [...messages]
+messageCopy.splice(index, 1)
+setMessages(messageCopy)
+        
+}
+
+
+return(
+
+    <div className="container">
+   { messages.map((message , index)=> {
+        return (
+        < Fragment key={index}>
+            <div className={`sms-${message.side}`} >
+                 <p>{message.text}</p>
+                <span>{message.author}</span>
+                <button onClick={() => { deleteMessage(index) }} className="btn">X</button>
+            </div>
+        </ Fragment>
+        )
+    }
+    )}
+    </div>
+
+
+
+)
+    
+}
+export default Messages 
